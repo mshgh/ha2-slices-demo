a17b212660c95a31ba37080de24bb0f7f3883a8e
mshgh <36337240+mshgh@users.noreply.github.com>
2018-10-31 19:48:24 +0100

    Empty hyperapp application

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-console.log('start');
+import '../style.css'
+
+import { h, app } from '../npm/hyperapp-v2'
+
+app({
+  init: {title: 'Hyperapp v2 - slices'},
+  view: state =>
+    h('div', {}, [
+      h('h2', {}, state.title)
+    ]),
+    container: document.getElementById('app'),
+});
