8944c3a8cf88e14edf7da51f1256d523005d6688
mshgh <36337240+mshgh@users.noreply.github.com>
2018-11-04 22:41:03 +0100

    modules.js updates
      - have sigle addModules method

--- src/lib/hyperapp/modules.js
+++ src/lib/hyperapp/modules.js
@@ -7,44 +7,44 @@ let state = {};
 let slices = {};
 let views = {};
 
-// TODO: simplier and cleaner implementation
-const add = (path, slice, init = {}) => {
-  if (slice.actions) {
-    const mapSlice = squirrel(path);
-
-    const sliceInfo = {};
-    const actions = Object.keys(slice.actions).reduce((actions, key) => {
-      const action = slice.actions[key];
-      actions[key] = (_, props, ev) => mapSlice(state => sliceInfo.state = action(props, ev)(state)); // ditch the state ;)
-      return actions;
-    }, {});
-
-    sliceInfo.api = slice.api ? slice.api(actions) : actions;
-    slices = mapSlice(_ => sliceInfo)(slices);
-    state = mapSlice(_ => sliceInfo.state = {...(slice.init || {}), ...init})(state);
-  }
-
-  if (slice.view) {
-    const pr = [...path].reverse();
-    const mapToProps = slice.mapToProps || (slices => ({ ...pr.reduce((o, k) => o[k], slices) }));
-    const view = (props, children) => slice.view({ ...mapToProps(slices), ...props }, children);
-
-    const [viewName, ...rest] = path;
-    const mapView = squirrel([viewName.replace(/^./, c => c.toUpperCase()), ...rest]);
-    views = mapView(_ => view)(views);
-  }
-};
-
-const buildSlice = (path, modules) => modules.forEach(module => {
+const addModules = (path, modules) => modules.forEach(module => {
   if (!Array.isArray(module)) state = squirrel(path)(_ => module)(state);
   else {
     const subPath = [...module[0].split('.').reverse(), ...path];
-    if (Array.isArray(module[1])) buildSlice(subPath, module[1]);
-    else add(subPath, module[1], module[2]); // TODO: make add part of buildSlice function
+    if (Array.isArray(module[1])) addModules(subPath, module[1]);
+    else {
+      // TODO: simplier and cleaner implementation
+      const slice = module[1];
+      const props = module[2] || {};
+      if (slice.actions) {
+        const mapSlice = squirrel(subPath);
+
+        const sliceInfo = {};
+        const actions = Object.keys(slice.actions).reduce((actions, key) => {
+          const action = slice.actions[key];
+          actions[key] = (_, props, ev) => mapSlice(state => sliceInfo.state = action(props, ev)(state)); // ditch the state ;)
+          return actions;
+        }, {});
+
+        sliceInfo.api = slice.api ? slice.api(actions) : actions;
+        slices = mapSlice(_ => sliceInfo)(slices);
+        state = mapSlice(_ => sliceInfo.state = { ...(slice.init || {}), ...props })(state);
+      }
+
+      if (slice.view) {
+        const pr = [...subPath].reverse();
+        const mapToProps = slice.mapToProps || (slices => ({ ...pr.reduce((o, k) => o[k], slices) }));
+        const view = (props, children) => slice.view({ ...mapToProps(slices), ...props }, children);
+
+        const [viewName, ...rest] = subPath;
+        const mapView = squirrel([viewName.replace(/^./, c => c.toUpperCase()), ...rest]);
+        views = mapView(_ => view)(views);
+      }
+    }
   }
 });
 
 export default (...modules) => {
-  buildSlice([], modules);
+  addModules([], modules);
   return { init: state, views };
 };
