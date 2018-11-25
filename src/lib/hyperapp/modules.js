9bf86c2291acd28f7654bc141009bc71acc515f5
mshgh <36337240+mshgh@users.noreply.github.com>
2018-11-25 10:20:27 +0100

    Added helper function to simplify code
      - isF - true if provided object is function
      - isA - true if provided object is array
      - toA - wraps non-array object into array
      - rA - returns reverted clone of provided array

--- src/lib/hyperapp/modules.js
+++ src/lib/hyperapp/modules.js
@@ -5,6 +5,11 @@ import squirrel from '../../npm/squirrel'
 
 let slices = {}, views = {};
 
+const isF = f => typeof f === 'function';
+const isA = a => Array.isArray(a);
+const toA = i => isA(i) ? i : [i];
+const rA = a => [...a].reverse();
+
 const addInit = (slice, map, { init }, props, acc) => !init && !props ? acc.init // TODO: move this check to caller method
   : map(s => slice.state = { ...s, ...(init || {}), ...(props || {}) })(acc.init);
 
@@ -20,9 +25,9 @@ const addApi = (slice, map, { actions, private: _actions, effects: getEffects })
     const effects = getEffects({ ...(slice.api || {}), ...bind(slice, map, _actions) });
     Object.keys(effects).forEach(key => {
       const effect = effects[key];
-      slice.api[key] = !Array.isArray(effect) ? (state, props) => [state, effect(props)]
-        : !Array.isArray(effect[0]) ? (state, props) => [effect[0](state, props), effect[1](props)]
-        : typeof effect[0][1] !== 'function' ? (state, props) => [effect[0][0](state, effect[0][1]), effect[1](props)]
+      slice.api[key] = !isA(effect) ? (state, props) => [state, effect(props)]
+        : !isA(effect[0]) ? (state, props) => [effect[0](state, props), effect[1](props)]
+        : !isF(effect[0][1]) ? (state, props) => [effect[0][0](state, effect[0][1]), effect[1](props)]
         : (state, props) => [effect[0][0](state, effect[0][1](props)), effect[1](props)];
     });
   }
@@ -31,29 +36,28 @@ const addApi = (slice, map, { actions, private: _actions, effects: getEffects })
 
 const addViews = (map, mapToProps, _views = {}) => Object.keys(_views).forEach(key => {
   const view = _views[key];
-  const fn = Array.isArray(view) ? (props, children) => view[1]({ ...view[0](slices), ...props }, children)
+  const fn = isA(view) ? (props, children) => view[1]({ ...view[0](slices), ...props }, children)
     : (props, children) => view({ ...mapToProps(slices), ...props }, children);
   views = squirrel([key], map)(_ => fn)(views) // TODO: mergse views?
 });
 
 const addSubs = (mapToProps, subs, seed = []) => subs.reduce((acc, item) => {
-  const isFn = typeof item == 'function';
-  acc.push([isFn ? mapToProps : item[0], isFn ? item : item[1]]);
+  acc.push(isF(item) ? [mapToProps, item] : item);
   return acc;
 }, seed);
 
 const addModules = (modules = [], basePath = [], seed = {}) => modules.reduce((acc, item) => {
-  if (!Array.isArray(item)) return { ...acc, init: squirrel([...basePath].reverse())(state => ({ ...state, ...item }))(acc.init) };
-  if (Array.isArray(item[1])) return addModules(item[1], [...basePath, ...item[0].split('.')], acc);
+  if (!isA(item)) return { ...acc, init: squirrel(rA(basePath))(state => ({ ...state, ...item }))(acc.init) };
+  if (isA(item[1])) return addModules(item[1], [...basePath, ...item[0].split('.')], acc);
 
   const slice = {}, [name, module, props] = item;
   const path = [...basePath, ...name.split('.')];
-  const map = squirrel([...path].reverse());
+  const map = squirrel(rA(path));
   acc.init = addInit(slice, map, module, props, acc);
   addApi(slice, map, module);
   const mapToProps = module.mapToProps || (slices => ({ ...path.reduce((o, k) => o[k], slices) }));
   if (module.views) addViews(map, mapToProps, module.views);
-  if (module.subscriptions) acc.subs = addSubs(mapToProps, Array.isArray(module.subscriptions) ? module.subscriptions : [module.subscriptions], acc.subs);
+  if (module.subscriptions) acc.subs = addSubs(mapToProps, toA(module.subscriptions), acc.subs);
   return acc;
 }, seed);
 
